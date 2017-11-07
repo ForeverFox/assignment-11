7a7e4a7ad41e2cf61d4e7bfd2b9da7d2596d4231
Forever Fox <cogden@mcad.edu>
2017-11-08 00:55:22 +0200

    Adding Instructions

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function () {
+	
+});
+
+/*
+
+// Add the attached icon image before the text in each LI element in the main navigation
+// Clicking on the H1 element in the header changes the background color of the header DIV
+// Change the text color of the first LI element in one of the sub-lists in the sidebar
+// Move the position of the even “numbered” LI elements in one of the sub-lists in the sidebar to the right by 5 pixels
+// Clicking on one of the post titles in the main content area replaces the content of the content DIV (with the ID of “content”) with content you specify
+// Clicking on one of the H2 elements in the sidebar toggles visibility of the list below it
+
+*/
+
+
+/* Assignment 10 code:
+
+$("document").ready(function() {
+	$("a").click(function(evt) {
+		$(this).addClass("new-background");
+	});
+
+	$("h2.title").click(function(evt) {
+		$(this).fadeOut(800, function(){
+
+		});
+	});
+});
+
+*/
